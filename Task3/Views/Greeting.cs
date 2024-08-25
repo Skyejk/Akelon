af7244e6803560e1b346c8c7c2ed7d7b3fcbf45b
Токугава Норимура <tokugava_norimura@mail.ru>
2024-08-26 01:35:43 +0300

    A welcome window has been added and the entry point to the application has been moved

---
+++ Task3/Views/Greeting.cs
@@ -0,0 +1,22 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3.Views
+{
+    internal class Greeting : IOLogical
+    {
+        public Greeting()
+        {
+            Output("Добро пожаловать в приложениен Akelon Task 3!");
+            Output("Для продолжения нажмите на любую клавишу.");
+            EnterAnyKey();
+
+            Menu menu = new Menu();
+            menu.MainMenu();
+        }
+    }
+}
